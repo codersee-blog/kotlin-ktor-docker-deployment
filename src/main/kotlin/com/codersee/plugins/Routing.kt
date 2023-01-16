c15f1e6dee67a53661891dfef893d999bc221df2
Piotr Wolak <contact@codersee.com>
2023-01-16 08:37:33 +0100

    Upload to remote

---
+++ src/main/kotlin/com/codersee/plugins/Routing.kt
@@ -0,0 +1,17 @@
+package com.codersee.plugins
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
+
